ec00d901e2420a1a1f1c6f68c8c9f1f3c64c26e4
Taoer <82244211+Little-six-Wang@users.noreply.github.com>
2023-11-23 17:21:43 +0800

    Update Read_vector.c
    这个程序通过MPI中的散射来读取矩阵或者向量

--- Read_vector.c
+++ Read_vector.c
@@ -4,16 +4,15 @@
 
 void createMatrix(int, int, double*);
 
-void Read_vector(double local_a[], int local_n, int m, int n,
-                char vec_name[], int my_rank, MPI_Comm comm){
-        double*a = NULL;
-	int i;
-	if(my_rank == 0){
-		a = malloc(n * m *(sizeof(double)));
-		createMatrix(m, n, a);
-		MPI_Scatter(a, local_n, MPI_DOUBLE, local_a, local_n, MPI_DOUBLE, 0, comm);
-		free(a);
-	}else{
-		MPI_Scatter(a, local_n, MPI_DOUBLE, local_a, local_n, MPI_DOUBLE, 0, comm);
-	}
+void Read_vector(double local_a[], int local_n, int m, int n, char vec_name[], int my_rank, MPI_Comm comm){
+  double*a = NULL;
+  int i;
+  if(my_rank == 0){
+    a = malloc(n * m *(sizeof(double)));
+    createMatrix(m, n, a);
+    MPI_Scatter(a, local_n, MPI_DOUBLE, local_a, local_n, MPI_DOUBLE, 0, comm);
+    free(a);
+  }else{
+    MPI_Scatter(a, local_n, MPI_DOUBLE, local_a, local_n, MPI_DOUBLE, 0, comm);
+  }
 }
