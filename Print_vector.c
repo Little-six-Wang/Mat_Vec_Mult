aff27c18bc9d12628acd910c4dfec23c51ea9c16
Taoer <82244211+Little-six-Wang@users.noreply.github.com>
2023-11-23 17:19:09 +0800

    Update Print_vector.c
    这个程序通过MPI中的聚集来打印结果

--- Print_vector.c
+++ Print_vector.c
@@ -2,22 +2,20 @@
 #include<mpi.h>
 #include<stdlib.h>
 
-void Print_vector(double local_b[], int local_n, int n, char title[],
-		int my_rank, MPI_Comm comm){
-	double*b = NULL;
-	int i;
-	if(my_rank == 0){
-		b = malloc(n * sizeof(double));
-		MPI_Gather(local_b, local_n, MPI_DOUBLE, b, local_n,
-				MPI_DOUBLE, 0, comm);
-		printf("%s\n", title);
-		for(i = 0; i < n; i++){
-			printf("%lf ", b[i]);
-		}
-		printf("\n");
-		free(b);
-	}else{
-		MPI_Gather(local_b, local_n, MPI_DOUBLE, b, local_n,
-				MPI_DOUBLE, 0, comm);
-	}
+void Print_vector(double local_b[], int local_n, int n, char title[], int my_rank, MPI_Comm comm){
+  double*b = NULL;
+  int i;
+  if(my_rank == 0){
+    b = malloc(n * sizeof(double));
+    MPI_Gather(local_b, local_n, MPI_DOUBLE, b, local_n, MPI_DOUBLE, 0, comm);
+    //printf("%s\n", title);
+    printf("Result = [\n");
+    for(i = 0; i < n; i++){
+      printf("%lf ", b[i]);
+    }
+    printf("]\n");
+    free(b);
+    }else{
+    MPI_Gather(local_b, local_n, MPI_DOUBLE, b, local_n, MPI_DOUBLE, 0, comm);
+  }
 }
